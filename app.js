fea473d7f7398f372b6f4a430e9481f89a807b90
aniket sharma <aniket965.as@gmail.com>
2017-07-21 13:38:15 +0530

    404page,resultpage ,many features

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var  app = express();
+var isResultFound = true;
+var isValidSearch = true; 
+/**
+ * set view engine
+ */
+app.set('view engine', 'ejs');
+
 /**
  * host static Files
  */
 app.use(express.static(__dirname + '/public'))
 
-/***
+/**
  * listen to port
  */
 app.listen(2000|process.env,()=>console.log("Listening port 2000"));
@@ -14,10 +21,62 @@ app.listen(2000|process.env,()=>console.log("Listening port 2000"));
 /**
  * Search request
  */
-
  app.get('/',(req,res)=>{
-    res.sendFile(__dirname+"/public/index1.html");
-    if(req.query.search !== undefined) {
-        console.log("Search for",req.query.search)
+    
+    /***
+     * Search Request will send result page
+     * if found 
+     * else will send not found page
+     * 
+     */
+    if (req.query.search !== undefined) {
+        
+        searchResult(req.query.search);
+        
+        if(isResultFound)    
+            res.render('result');
+        
+        else if(isValidSearch)
+                updateApi();
+        else
+            res.render('404')
+
+
     }
+
+
+    /**
+     * sends home page if it is not a search request
+     */    
+    else
+        res.sendFile(__dirname+"/public/index1.html");
  });
+
+
+
+
+
+/**
+ * Checks rollnumber
+ * Search for result in api
+ */
+ function searchResult(rollnumber){
+     /**
+      * Checks if query is valid rollnumber or not
+      */
+    if(/^\d{11}$/.test(rollnumber)){
+        isResultFound = true
+    }
+     else {
+        isResultFound = false
+        isValidSearch = false
+    }
+    
+ }
+
+/**
+ * Updates the api
+ */
+ function updateApi() {
+
+ }
