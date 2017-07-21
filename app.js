4c0d9b68927fa20bde2af0663a5354490e012758
aniket sharma <aniket965.as@gmail.com>
2017-07-21 12:24:18 +0530

    basic app

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 var express = require('express');
 var  app = express();
+/**
+ * host static Files
+ */
+app.use(express.static(__dirname + '/public'))
 
+/***
+ * listen to port
+ */
 app.listen(2000|process.env,()=>console.log("Listening port 2000"));
+
+
+/**
+ * Search request
+ */
+
+ app.get('/',(req,res)=>{
+    res.sendFile(__dirname+"/public/index1.html");
+    console.log(req.query.search);
+ });
