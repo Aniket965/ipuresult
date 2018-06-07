29c225155c2e96b619e61ad7a43d01e2e8b6971e
aniket965 <aniket965.as@gmail.com>
2018-06-07 16:52:41 +0530

    added What and why Page

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
-var express = require("express");
-var request = require("request");
-const { findCode } = require("ipu-core-code");
-var app = express();
-var isResultFound = false;
-var isValidSearch = true;
-var studentdata;
+var express = require('express')
+var request = require('request')
+const { findCode } = require('ipu-core-code')
+var app = express()
+var isResultFound = false
+var isValidSearch = true
+var studentdata
 
-var d = new Date();
-var n = d.getFullYear() + "";
-var yearcode = parseInt(n.substring(2, 4));
+var d = new Date()
+var n = d.getFullYear() + '';
+var yearcode = parseInt(n.substring(2, 4))
 var month = d.getMonth()
 /**
  * set view engine
  */
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs')
 
 /**
  * host static Files
  */
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'))
 
 /**
  * listen to port
  */
-app.listen(process.env.PORT || 2000, () => console.log("Listening port 2000"));
+app.listen(process.env.PORT || 2000, () => console.log('Listening port 2000'))
 
-var roll;
+var roll
 
-function findSubjectCode(id, subjectkeys) {
+function findSubjectCode (id, subjectkeys) {
   if (subjectkeys) {
-    let ids = Object.keys(subjectkeys);
-    let name = "";
+    let ids = Object.keys(subjectkeys)
+    let name = '';
     ids.forEach(uid => {
-      if (uid.includes(id)) name = initials(subjectkeys[uid]);
-    });
-    return name;
-  } else return "";
+      if (uid.includes(id)) name = initials(subjectkeys[uid])
+    })
+    return name
+  } else return '';
 }
-
 
 /**
  * Search request
  */
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   /***
    * Search Request will send result page
    * if found
@@ -50,108 +49,108 @@ app.get("/", (req, res) => {
    *
    */
   if (req.query.search !== undefined) {
-    searchResult(res, req.query.search.toString());
+    searchResult(res, req.query.search.toString())
   } else
     /**
      * sends home page if it is not a search request
      */
-    res.sendFile(__dirname + "/public/index1.html");
-});
+    {res.sendFile(__dirname + "/public/index1.html");}
+})
 
 /**
  * Checks rollnumber
  * Search for result in api
  */
 
-function searchResult(res, rollnumber) {
+function searchResult (res, rollnumber) {
   /**
    * Checks if query is valid rollnumber or not
    */
   if (/^\d{11}$/.test(rollnumber)) {
-    isResultFound = true;
-    findApi(res, rollnumber);
+    isResultFound = true
+    findApi(res, rollnumber)
   } else {
-    isResultFound = false;
-    isValidSearch = false;
-    res.render("404");
+    isResultFound = false
+    isValidSearch = false
+    res.render('404')
   }
 }
 
 /**
  * Updates the api
  */
-function updateApi() {
-  request("https://btechipuresults.herokuapp.com/update");
-  console.log("updating");
-  request("https://whoami-ani965.herokuapp.com/update");
+function updateApi () {
+  request('https://btechipuresults.herokuapp.com/update')
+  console.log('updating')
+  request('https://whoami-ani965.herokuapp.com/update')
 }
 
 /**
  * finds api code through specific algoruth
  */
 
-function findApi(res, rollnumber) {
+function findApi (res, rollnumber) {
   /**
    *
    * callcode 1 is for aniket965/res repo
    * callcode 0  for ipuresults/btech
    */
-  var callcode = 0;
-  let code = findCode(rollnumber, yearcode, month, 0);
-  rollnumber.indexOf("039") === 6 ? (callcode = 1) : (callcode = 0);
-  getDataFromApi(res, rollnumber, code, callcode, yearcode);
+  var callcode = 0
+  let code = findCode(rollnumber, yearcode, month, 0)
+  rollnumber.indexOf('039') === 6 ? (callcode = 1) : (callcode = 0)
+  getDataFromApi(res, rollnumber, code, callcode, yearcode)
 }
 
 /**
  * calls apis
  */
 
-function getDataFromApi(res, rn, code, callcode, yearcode) {
+function getDataFromApi (res, rn, code, callcode, yearcode) {
   if (callcode === 0) {
-    if (code.substr(1, 3) === "074") {
-      code = "6036" + yearcode;
+    if (code.substr(1, 3) === '074') {
+      code = '6036' + yearcode
     }
     request(`https://raw.githubusercontent.com/ipuresults/btech/master/api/${code}.json`,
-      function(error, response, body) {
+      function (error, response, body) {
         if (!error && response.statusCode == 200) {
-          var info = JSON.parse(body);
+          var info = JSON.parse(body)
           if (info[rn] !== undefined) {
-            roll = rn;
+            roll = rn
           let r = info[rn]
-          r['codes'] = info['subject_codes']
-            res.render("result", r);
+            r['codes'] = info['subject_codes']
+            res.render('result', r)
           } else {
-            res.render("404");
+            res.render('404')
           }
         } else if (response.statusCode !== 200) {
-          res.render("404");
-          updateApi();
+          res.render('404')
+          updateApi()
         } else {
-          res.render("404");
+          res.render('404')
         }
       }
-    );
+    )
   } else if (callcode === 1) {
     request(
-      "https://raw.githubusercontent.com/aniket965/res/master/api/" +
+      'https://raw.githubusercontent.com/aniket965/res/master/api/' +
         code +
-        ".json",
-      function(error, response, body) {
+        '.json',
+      function (error, response, body) {
         if (!error && response.statusCode == 200) {
-          var info = JSON.parse(body);
+          var info = JSON.parse(body)
           if (info[rn] !== undefined) {
-            roll = rn;
+            roll = rn
             let r = info[rn]
             let subcodes = info['subject_codes']
-            res.render("result", r);
-          } else res.render("404");
+            res.render('result', r)
+          } else res.render('404')
         } else if (response.statusCode !== 200) {
-          res.render("404");
-          updateApi();
+          res.render('404')
+          updateApi()
         } else {
-          res.render("404");
+          res.render('404')
         }
       }
-    );
+    )
   }
 }
--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,10 @@
 
 function search() {
   let q = document.getElementById('a').value
-  window.location= `/?search=${q}`
+  window.location = `/?search=${q}`
+}
+
+function scrolltodoc() {
+  let offset = document.getElementById('aboutpage').offsetTop;
+  window.scrollTo(0, offset)
 }
